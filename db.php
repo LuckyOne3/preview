f1d965844e3e26c4d3f20b5e4999cf13fff75c21
LuckyOne3 <35265338+LuckyOne3@users.noreply.github.com>
2020-02-20 17:11:44 +0300

    Create db.php

---
+++ db.php
@@ -0,0 +1,32 @@
+<?php
+
+
+$connection = mysqli_connect(',,,');
+mysqli_set_charset($connection, 'utf8');
+
+
+
+if ($connection == false)
+{
+  echo 'Не удалось подключиться к базе данных!<br>';
+  echo mysqli_connect_error();
+  exit();
+
+}
+if (!function_exists("clean")) {
+    if (get_magic_quotes_gpc()) {
+        function magicquotes_Stripslashes(&$value, $key) {
+            $value = stripslashes($value);
+        }
+        $gpc = array(&$_COOKIE, &$_REQUEST);
+        array_walk_recursive($gpc, 'magicquotes_Stripslashes');
+    }
+    function clean(&$value, $key) {
+//эта функция экранирует все кавычки.
+        $value = mysqli_real_escape_string($value);
+    }
+}
+$req = array(&$_REQUEST);
+array_walk_recursive($req, 'clean');
+
+?>
